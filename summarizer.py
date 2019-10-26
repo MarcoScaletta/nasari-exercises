59e4219f57d95ffcf89122493b36a4a8ab9737ee
Marco Scaletta <scalettamarco@yahoo.it>
2019-10-26 18:52:16 +0200

    add paragraph nearness summarization

--- summarizer.py
+++ summarizer.py
@@ -13,6 +13,7 @@ class Summarizer:
         self.preprocessor = preprocessor
         self.document = Document(filename, self.preprocessor)
         self.init_no_weights()
+        self.sorted_paragraphs_dict = dict()
 
     def title_summarize(self):
         pass
@@ -34,10 +35,6 @@ class Summarizer:
         self.sorted_paragraphs = list(np.argsort(par_val))
         self.sorted_paragraphs.reverse()
 
-    def init_weights(self):
-        # nasari_context = [[word, 0] self.document.title_bow]
-        self.init_vectors()
-        
     def summarize(self, percentage):
         tot_lines = int(len(self.document.lines)*percentage)
         if tot_lines < 1:
@@ -48,15 +45,22 @@ class Summarizer:
             return range(len(self.document.paragraphs))
         else:
             print("Summarizing to", tot_lines, "lines")
-        new_lines = self.sorted_paragraphs[:tot_lines]
-        new_lines.sort()
-        return [self.document.paragraphs[i] for i in new_lines]
+
+        # new_lines = np.zeros(len(self.document.paragraphs))
+
+        # for method in self.sorted_paragraphs_dict:
+        #     print("method:", method)
+        #     for i in range(len(new_lines)):
+        #         new_lines[i] += self.sorted_paragraphs_dict
+        
+        # # new_lines = self.sorted_paragraphs[:tot_lines]
+        # # new_lines.sort()
+
+        # return [self.document.paragraphs[i] for i in new_lines]
 
     
     def best_nasari_vector_id(self, word):
         vectors = self.nasari_vectors(word)
-        # l = list(vectors.keys())
-        # l.sort()
         if vectors is None:
             return None
         if len(vectors) == 1:
@@ -83,47 +87,20 @@ class Summarizer:
         doc_bow = self.document.text_bow
         return doc_bow.intersection(set(nasari_vector.keys()))
 
-    def init_vectors(self):
-        self.vectors_id = dict()
-        for word in self.document.text_bow:
-            best_vector_id = self.best_nasari_vector_id(word)
-            if best_vector_id is not None:
-                self.vectors_id[word] = best_vector_id
-
-    # def wo(self, v1, v2):
-    #     wo = 0
-    #     if v1 is not None and v2 is not None:
-    #         print(v1)
-    #         print(v2)
-    #         overlap_dimensions = list(set(v1.keys()).intersection(set(v2.keys())))
-    #         rank_sum = 0
-    #         index_sum = 0
-    #         for i in range(len(overlap_dimensions)):
-    #             q = overlap_dimensions[i]
-    #             rank_sum += float(1)/float(v1[q] + v2[q])
-    #             if i > 0:
-    #                 index_sum += float(1)/float(2*i)
-    #         index_sum += float(1)/float(2*len(overlap_dimensions))
-    #         print(rank_sum)
-    #         print(index_sum)
-    #         if len(overlap_dimensions) > 0:
-    #             wo = rank_sum/index_sum
-    #     return wo
-
-    # def sim(self, w1, w2):
-    #     # if w1 == w2:
-    #     #     return 1
-    #     # else:
-    #     max_sim = 0
-    #     w1_vectors = self.nasari_vectors(w1)
-    #     w2_vectors = self.nasari_vectors(w2)
-    #     if w1_vectors is None or w2_vectors is None:
-    #         return 0
-    #     for v1 in w1_vectors:
-    #         for v2 in w2_vectors:
-    #             # sim = math.sqrt(self.wo(w1_vectors[v1], w2_vectors[v2]))
-    #             sim = math.sqrt(self.wo(w1_vectors[v1], w2_vectors[v2]))
-    #             if sim > max_sim:
-    #                 max_sim = sim
-    #     return sim
-            
+    def init_nearness(self):
+        self.nearness_list = list(np.zeros(len(self.document.paragraphs_bow)))
+        title_bow = self.document.title_bow
+        for i in range(len(self.document.paragraphs_bow)):
+            bow_i = self.document.paragraphs_bow[i]
+            nearness = self.nasari.nearness(bow_i, title_bow)
+            self.nearness_list[i] += nearness
+            for j in range(len(self.document.paragraphs_bow)):
+                if i < j:
+                    bow_j = self.document.paragraphs_bow[j]
+                    nearness = self.nasari.nearness(bow_i, bow_j)
+                    self.nearness_list[i] += nearness
+                    self.nearness_list[j] += nearness
+        
+        self.sorted_paragraphs_dict['NEARNESS'] = list(np.argsort(self.nearness_list))
+        self.sorted_paragraphs_dict['NEARNESS'].reverse()
+        print(self.sorted_paragraphs_dict['NEARNESS'])
