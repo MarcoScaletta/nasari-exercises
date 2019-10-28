b1be250e2a64ce8b75ff92e76e90d499383ade37
Marco Scaletta <scalettamarco@yahoo.it>
2019-10-28 09:07:48 +0100

    update print + save results on file

--- app.py
+++ app.py
@@ -14,13 +14,23 @@ filename.append("The-Last-Man-on-the-Moon--Eugene-Cernan-gives-a-compelling-acco
 
 new_sizes = [0.1, 0.2, 0.3]
 
+results = open("results", "w")
+
 for f in filename:
-    summarizer = Summarizer(nasari, preprocessor, f)
+    summarizer = Summarizer(nasari, preprocessor, f, ['TITLE','NEARNESS'])
     print()
+    results.write("\n")
     print("TITLE:", summarizer.document.title.upper())
+    results.write("TITLE:"+ summarizer.document.title.upper())
     print()
+    results.write("\n")
+    results.write("\n")
+
     for new_size in new_sizes:
         summarized_doc_lines = summarizer.summarize(new_size)
         for i in range(len(summarized_doc_lines)):
             print(str(i+1)+".", summarized_doc_lines[i])
+            results.write(str(i+1)+"."+ str(summarized_doc_lines[i]))
+            results.write("\n")
         print()
+        results.write("\n")
